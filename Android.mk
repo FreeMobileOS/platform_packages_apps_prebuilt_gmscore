5775b8518c2f3dabdaf65422ebbad99c4a758ca5
Satish Patel <tosatishpatel@gmail.com>
2019-06-18 13:19:02 +0530

    Grant Fake signature permission by default on boot
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- Android.mk
+++ Android.mk
@@ -14,14 +14,25 @@
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-com.google.android.gms.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
+
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := GmsCore
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := GmsCore
-
-LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := GmsCore.apk
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-
+LOCAL_MODULE_PATH = $(TARGET_OUT)/priv-app
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_OVERRIDES_PACKAGES := com.qualcomm.location
+LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml
 include $(BUILD_PREBUILT)
